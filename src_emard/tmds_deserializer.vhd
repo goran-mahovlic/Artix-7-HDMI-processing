f01660f1b7a6e2fe89f6724d09338aff75b37053
emard <vordah@gmail.com>
2019-02-03 00:36:35 +0100

    tmds_deserializer with configurable phase of the latch instance

--- src_emard/tmds_deserializer.vhd
+++ src_emard/tmds_deserializer.vhd
@@ -15,10 +15,12 @@ port
 end;
 
 architecture Behavioral of tmds_deserializer is
-    constant C_latch_cycle: integer := -2; -- must latch data 2 clk_shift cycles earlier to have output in sync
-    signal R_shift_r, R_latch_r, R_data_r: std_logic_vector(9 downto 0);
-    signal R_shift_g, R_latch_g, R_data_g: std_logic_vector(9 downto 0);
-    signal R_shift_b, R_latch_b, R_data_b: std_logic_vector(9 downto 0);
+    -- latch data 1 clk_shift cycle earlier to have output in sync with clk_pixel
+    constant C_latch_phase: integer := -1; -- default -1 (positive: later, negative: earlier)
+    signal R_shift_c, R_latch_c, R_data_c: std_logic_vector(9 downto 0); -- clock
+    signal R_shift_r, R_latch_r, R_data_r: std_logic_vector(9 downto 0); -- red
+    signal R_shift_g, R_latch_g, R_data_g: std_logic_vector(9 downto 0); -- green
+    signal R_shift_b, R_latch_b, R_data_b: std_logic_vector(9 downto 0); -- blue
     constant C_shift_clock_initial: std_logic_vector(9 downto 0) := "0000011111";
     signal R_clock: std_logic_vector(9 downto 0) := C_shift_clock_initial;
     signal R_shift_clock_in_sync: std_logic := '1';
@@ -29,18 +31,20 @@ begin
     process(clk_shift)
     begin
       if rising_edge(clk_shift) then
+        R_shift_c <= tmds_p(3) & R_shift_c(R_shift_c'high downto 1);
         R_shift_r <= tmds_p(2) & R_shift_r(R_shift_r'high downto 1);
         R_shift_g <= tmds_p(1) & R_shift_g(R_shift_g'high downto 1);
         R_shift_b <= tmds_p(0) & R_shift_b(R_shift_b'high downto 1);
-        if R_clock(5+C_latch_cycle downto 4+C_latch_cycle) = C_shift_clock_initial(5 downto 4) then
-          -- 1st stage pipeline
+        if R_shift_c(5+C_latch_phase downto 4+C_latch_phase) = C_shift_clock_initial(5 downto 4) then
           R_latch_r <= R_shift_r;
           R_latch_g <= R_shift_g;
           R_latch_b <= R_shift_b;
         end if;
       end if;
     end process;
-
+    
+    -------------8<------------------------8<-------------
+    G_unused: if false generate
     -- every N off-sync cycles of clk_shift: signal to skip 1 cycle in order to get in sync
     process(clk_shift)
     begin
@@ -75,6 +79,8 @@ begin
         end if;
       end if;
     end process;
+    end generate; -- G_unused
+    -------------8<------------------------8<-------------
     
     -- clk_pixel synchronous output
     process(clk_pixel)
@@ -83,7 +89,7 @@ begin
         R_data_r <= R_latch_r;
         R_data_g <= R_latch_g;
         R_data_b <= R_latch_b;
-        -- clk_pixel synchronous check: is R_clock in sync?
+        -- clk_pixel synchronous check: is R_clock in sync with clk_pixel?
         if R_clock(5 downto 4) = C_shift_clock_initial(5 downto 4) then
           R_shift_clock_in_sync <= '1';
         else
