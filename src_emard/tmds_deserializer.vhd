2544565997134c2144073e7512cf42545ec30387
emard <vordah@gmail.com>
2019-02-02 23:51:25 +0100

    tmds_deserializer latching data 2 cycles earlier to have data output in sync

--- src_emard/tmds_deserializer.vhd
+++ src_emard/tmds_deserializer.vhd
@@ -1,9 +1,9 @@
 -- TODO testing
 
-library IEEE;
-use IEEE.std_logic_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.std_logic_arith.ALL;
+use ieee.std_logic_unsigned.ALL;
 
 entity tmds_deserializer is
 port
@@ -15,12 +15,13 @@ port
 end;
 
 architecture Behavioral of tmds_deserializer is
+    constant C_latch_cycle: integer := -2; -- must latch data 2 clk_shift cycles earlier to have output in sync
     signal R_shift_r, R_latch_r, R_data_r: std_logic_vector(9 downto 0);
     signal R_shift_g, R_latch_g, R_data_g: std_logic_vector(9 downto 0);
     signal R_shift_b, R_latch_b, R_data_b: std_logic_vector(9 downto 0);
     constant C_shift_clock_initial: std_logic_vector(9 downto 0) := "0000011111";
-    signal R_clock : std_logic_vector(9 downto 0) := C_shift_clock_initial;
-    signal R_shift_clock_off_sync: std_logic := '0';
+    signal R_clock: std_logic_vector(9 downto 0) := C_shift_clock_initial;
+    signal R_shift_clock_in_sync: std_logic := '1';
     signal R_shift_clock_synchronizer: std_logic_vector(7 downto 0) := (others => '0');
     signal R_sync_fail: std_logic_vector(6 downto 0); -- counts sync fails, after too many, reinitialize shift_clock
 begin
@@ -31,7 +32,8 @@ begin
         R_shift_r <= tmds_p(2) & R_shift_r(R_shift_r'high downto 1);
         R_shift_g <= tmds_p(1) & R_shift_g(R_shift_g'high downto 1);
         R_shift_b <= tmds_p(0) & R_shift_b(R_shift_b'high downto 1);
-        if R_clock(5 downto 4) = C_shift_clock_initial(5 downto 4) then
+        if R_clock(5+C_latch_cycle downto 4+C_latch_cycle) = C_shift_clock_initial(5 downto 4) then
+          -- 1st stage pipeline
           R_latch_r <= R_shift_r;
           R_latch_g <= R_shift_g;
           R_latch_b <= R_shift_b;
@@ -43,9 +45,9 @@ begin
     process(clk_shift)
     begin
       if rising_edge(clk_shift) then
-        if R_shift_clock_off_sync = '1' then
+        if R_shift_clock_in_sync = '0' then
 	  if R_shift_clock_synchronizer(R_shift_clock_synchronizer'high) = '1' then
-            R_shift_clock_synchronizer <= (others => '0');
+            R_shift_clock_synchronizer <= (others => '0'); -- single clk_shift pulse
           else
             R_shift_clock_synchronizer <= R_shift_clock_synchronizer + 1;
           end if;
@@ -83,9 +85,9 @@ begin
         R_data_b <= R_latch_b;
         -- clk_pixel synchronous check: is R_clock in sync?
         if R_clock(5 downto 4) = C_shift_clock_initial(5 downto 4) then
-          R_shift_clock_off_sync <= '0';
+          R_shift_clock_in_sync <= '1';
         else
-          R_shift_clock_off_sync <= '1';
+          R_shift_clock_in_sync <= '0';
         end if;
       end if;
     end process;
