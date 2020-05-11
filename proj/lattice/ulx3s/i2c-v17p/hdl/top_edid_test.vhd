cdfcc379a21abf618987251638b14c6fe46b4690
Goran Mahovlic <goran.mahovlic@gmail.com>
2020-05-11 11:41:47 +0200

    Adding working I2C EDID with new lpf

--- proj/lattice/ulx3s/i2c-v17p/hdl/top_edid_test.vhd
+++ proj/lattice/ulx3s/i2c-v17p/hdl/top_edid_test.vhd
@@ -28,7 +28,7 @@ Port
         --hdmi_rx_p     : in    std_logic_vector(2 downto 0);
 
   -- Digital Video (differential inputs)
-  gpdi_dp, gpdi_dn: in std_logic_vector(3 downto 0);
+  gpdi_dp, gpdi_dn: out std_logic_vector(3 downto 0);
   -- control lines as input with pullups to activate hotplug autodetection
   -- to enable hotplug, gpdi_ethn capacitor should be bypassed by 470 ohm resistor
   -- it's a C closest to the DIP switch
@@ -38,9 +38,12 @@ Port
   -- i2c shared for digital video and RTC
   gpdi_scl: in std_logic;
   gpdi_sda: inout std_logic;
+  gn8: inout std_logic;
+  gp: inout std_logic_vector(27 downto 13);
+  gpa: in std_logic_vector(12 downto 9);
+  gpb: inout std_logic_vector(8 downto 0); 
+  gn: inout std_logic_vector(27 downto 13);
 
-  gp, gn: in std_logic_vector(27 downto 0);
-  
         --- HDMI out
         --hdmi_tx_cec   : inout std_logic;
         --hdmi_tx_clk_n : out   std_logic;
@@ -65,33 +68,34 @@ begin
     led <= debug;
     wifi_gpio0 <= btn(0);
     gpdi_ethn <= '1' when btn(0) = '1' else '0';
+    gn(13) <= '1' when btn(1) = '0' else '1'; -- eth- hotplug
 
     clk_25_inst: entity work.clk_25
     port map
     (
-        clki => clk_25mhz,
-        clks3 => clk100,
-        locked => open
+        CLKI => clk_25mhz,
+        CLKOS3 => clk100,
+        LOCK => open
     );
     
     diff_in_inst: ilvds
     port map
     (
-      a  => gp(12),
-      an => gn(12),
+      a  => gpa(12),
+      an => not gpa(12),
       z  => clk_pixel_cable
     );
 
-    clk_raw_cable <= not gpdi_dn(3); -- emulate differential, force insertion of fpga fabric
+    -- clk_raw_cable <= not gpdi_dn(3); -- emulate differential, force insertion of fpga fabric
     -- clk_raw_cable <= gpdi_dp(3) and not gpdi_dn(3); -- emulate differential, force insertion of fpga fabric
 
     clk_video_inst: entity work.clk_25
     port map
     (
-        clki => clk_raw_cable, -- trying to use single ended mode
-        clks1 => clk_shift,
-        clks2 => clk_pixel,
-        locked => locked
+        CLKI => clk_raw_cable, -- trying to use single ended mode
+        CLKOS => clk_shift,
+        CLKOS2 => clk_pixel,
+        LOCK => locked
     );
     
     blink_inst: entity work.blink
@@ -106,12 +110,16 @@ begin
     debug(6) <= blink(7);
     debug(7) <= locked;
 
+    gpdi_dp <= gpa(12 downto 9);
+
     edid_rom_inst: entity work.edid_rom
     port map
     (
         clk        => clk100,
-        sclk_raw   => gpdi_scl,
-        sdat_raw   => gpdi_sda,
+        sclk_raw   => gn8,
+        sdat_raw   => gpb(8),
+        -- sclk_raw   => gpdi_scl,
+        -- sdat_raw   => gpdi_sda,
         edid_debug => debug(2 downto 0)
     );
 
